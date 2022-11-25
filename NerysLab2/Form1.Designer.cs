10cbdcfb6efc1afbceacf542dd24221177678033
Rodrigo Nerys <nerys.rodrigo@gmail.com>
2022-11-24 22:34:10 -0400

    Initial commit

---
+++ NerysLab2/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace NerysLab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMultiplier = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bntReset = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblTopic = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnData = new System.Windows.Forms.Button();
+            this.btnPicture = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtMultiplier);
+            this.groupBox1.Controls.Add(this.txtNumber);
+            this.groupBox1.Controls.Add(this.lblAnswer);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.bntReset);
+            this.groupBox1.Controls.Add(this.btnCalc);
+            this.groupBox1.Location = new System.Drawing.Point(10, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(233, 305);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Multiplier";
+            // 
+            // txtMultiplier
+            // 
+            this.txtMultiplier.Location = new System.Drawing.Point(92, 106);
+            this.txtMultiplier.Name = "txtMultiplier";
+            this.txtMultiplier.Size = new System.Drawing.Size(118, 22);
+            this.txtMultiplier.TabIndex = 1;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(92, 42);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(118, 22);
+            this.txtNumber.TabIndex = 0;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAnswer.Location = new System.Drawing.Point(92, 170);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(118, 22);
+            this.lblAnswer.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Answer:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Multiplier:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number:";
+            // 
+            // bntReset
+            // 
+            this.bntReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bntReset.Location = new System.Drawing.Point(124, 250);
+            this.bntReset.Name = "bntReset";
+            this.bntReset.Size = new System.Drawing.Size(90, 40);
+            this.bntReset.TabIndex = 3;
+            this.bntReset.Text = "&Reset";
+            this.bntReset.UseVisualStyleBackColor = true;
+            this.bntReset.Click += new System.EventHandler(this.bntReset_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(16, 250);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(90, 40);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "&Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblTopic);
+            this.groupBox2.Controls.Add(this.btnExit);
+            this.groupBox2.Controls.Add(this.btnHide);
+            this.groupBox2.Controls.Add(this.btnData);
+            this.groupBox2.Controls.Add(this.btnPicture);
+            this.groupBox2.Controls.Add(this.picBox);
+            this.groupBox2.Controls.Add(this.lblInfo);
+            this.groupBox2.Location = new System.Drawing.Point(262, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(445, 305);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Information";
+            // 
+            // lblTopic
+            // 
+            this.lblTopic.AutoSize = true;
+            this.lblTopic.Location = new System.Drawing.Point(171, 18);
+            this.lblTopic.Name = "lblTopic";
+            this.lblTopic.Size = new System.Drawing.Size(95, 16);
+            this.lblTopic.TabIndex = 5;
+            this.lblTopic.Text = "Saint John City";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(339, 250);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(90, 40);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(232, 250);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(90, 40);
+            this.btnHide.TabIndex = 2;
+            this.btnHide.Text = "&Hide";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // btnData
+            // 
+            this.btnData.Location = new System.Drawing.Point(125, 250);
+            this.btnData.Name = "btnData";
+            this.btnData.Size = new System.Drawing.Size(90, 40);
+            this.btnData.TabIndex = 1;
+            this.btnData.Text = "&Data";
+            this.btnData.UseVisualStyleBackColor = true;
+            this.btnData.Click += new System.EventHandler(this.btnData_Click);
+            // 
+            // btnPicture
+            // 
+            this.btnPicture.Location = new System.Drawing.Point(18, 250);
+            this.btnPicture.Name = "btnPicture";
+            this.btnPicture.Size = new System.Drawing.Size(90, 40);
+            this.btnPicture.TabIndex = 0;
+            this.btnPicture.Text = "&Picture";
+            this.btnPicture.UseVisualStyleBackColor = true;
+            this.btnPicture.Click += new System.EventHandler(this.btnPicture_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblInfo.Location = new System.Drawing.Point(18, 42);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(413, 186);
+            this.lblInfo.TabIndex = 4;
+            this.lblInfo.Visible = false;
+            // 
+            // picBox
+            // 
+            this.picBox.Image = ((System.Drawing.Image)(resources.GetObject("picBox.Image")));
+            this.picBox.Location = new System.Drawing.Point(18, 42);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(413, 186);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox.TabIndex = 6;
+            this.picBox.TabStop = false;
+            this.picBox.Visible = false;
+            this.picBox.Click += new System.EventHandler(this.picBox_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnPicture;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.bntReset;
+            this.ClientSize = new System.Drawing.Size(719, 331);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab 2 By Rodrigo Nerys";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button bntReset;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button btnData;
+        private System.Windows.Forms.Button btnPicture;
+        private System.Windows.Forms.TextBox txtMultiplier;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label lblTopic;
+        private System.Windows.Forms.PictureBox picBox;
+    }
+}
+
---
+++ NerysLab2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Rodrigo Nerys, Oct 03, 2022.
+// In this Lab 2, I am creating an executable program that will calculate integer numbers and
+// show a picture and info about Saint John.
+namespace NerysLab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Close the form
+            this.Close();
+        }
+
+        private void btnPicture_Click(object sender, EventArgs e)
+        {
+            //show the picture
+            picBox.Visible = true;
+            
+            //hide the label
+            lblInfo.Hide();
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            //hide the picture and the data info
+            picBox.Hide();
+            lblInfo.Hide();
+        }
+
+        private void bntReset_Click(object sender, EventArgs e)
+        {
+            //blank out the text box and answer label
+            txtNumber.ResetText();
+            txtMultiplier.ResetText();
+            lblAnswer.ResetText();
+            
+            //put the cursor on the top text box
+            txtNumber.Focus();
+        }
+
+        private void btnData_Click(object sender, EventArgs e)
+        {
+            //show info label
+            lblInfo.Visible = true;
+            
+            //information in the data label
+            lblInfo.Text = "My topic is " + lblTopic.Text + ".\n\n" +
+                "Saint John is a seaport city of the Atlantic Ocean located on the Bay of Fundy in the province of New Brunswick, Canada. " +
+                "Saint John is the oldest incorporated city in Canada, established by royal charter on May 18, 1785.\n" +
+                "This photo is a view of Uptown and the Harbor of Saint John, from the City Sign lookout. One of the most beaultiful places to visit in the city!";
+            
+            //hide picture box
+            picBox.Hide();
+        }
+
+        private void picBox_Click(object sender, EventArgs e)
+        {
+            //Display a message box
+            MessageBox.Show("Photo of my bike and the bay!", "Rodrigo Nerys", MessageBoxButtons.OK);
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            //create local variables
+            int number, multiplier, answer;
+
+            if(txtNumber.Text != "") //compare if number text box is empty
+            {
+                //Place the data from the textbox into the variables
+                number = Convert.ToInt32(txtNumber.Text);
+            } else number = 0;
+
+            if (txtMultiplier.Text != "") //compare if multiplier text box is empty
+            {
+                //Place the data from the textbox into the variables
+                multiplier = Convert.ToInt32(txtMultiplier.Text);
+            } else multiplier = 0;
+            
+
+            //multiply the number and the multiplier
+            answer = number * multiplier;
+                
+            //display the answer
+            lblAnswer.Text = answer.ToString();
+            
+        }
+    }
+}
